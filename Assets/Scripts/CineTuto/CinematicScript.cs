b47cdbea6e8469398fcb3bd030a9de64685fcff3
CakeTheSnake <antagarcia003@gmail.com>
2023-09-25 03:31:17 +0200

    cinematicas

---
+++ Assets/Scripts/CineTuto/CinematicScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CinematicScript : MonoBehaviour
+{
+    public List<GameObject> textosCarta;
+
+    private int index = 0;
+    private bool canShowText = false;
+
+    public void Start()
+    {
+        StartCoroutine(WaitForAnimationToEnd());
+    }
+
+    IEnumerator WaitForAnimationToEnd()
+    {
+        yield return new WaitForSeconds(5.5f);
+        canShowText = true;
+        textosCarta[index].SetActive(true);
+    }
+
+    private void ShowNextText()
+    {
+        textosCarta[index].SetActive(false);
+        index++;
+        textosCarta[index].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown && canShowText)
+        {
+            if(index < textosCarta.Count-1)
+            {
+                ShowNextText();
+            }
+            else
+            {
+                SceneManager.LoadScene("Tutorial");
+            }
+        }
+    }
+}
